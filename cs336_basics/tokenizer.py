d548d30ec5655a680f2135092883b33704f277ec
tuanyuan2008 <schen1337@gmail.com>
2025-07-15 18:25:26 -0400

    Lint, rename vars

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,10 +1,15 @@
+"""
+BPE tokenizer that loads a provided vocabulary and list of merges and uses them to encode
+and decode text to/from token IDs.
+"""
+
 from typing import Iterable
 from collections.abc import Iterator
 from cs336_basics.bpe_helper import pretokenize, pretokenize_iter
 
 class Tokenizer():
     """
-    Tokenizer using BPE to tokenize text.
+    BPE tokenizer that loads a provided vocabulary and list of merges and uses them to encode and decode text to/from token IDs.
     """
     def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens: list[str] | None = None):
         self.vocab = vocab
@@ -13,12 +18,12 @@ class Tokenizer():
             self.reverse_vocab[v] = k
         self.merges = set(merges)
         self.special_tokens = special_tokens
-
+    
     @classmethod
     def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens: list[str] | None = None):
         """
         Class method that constructs and return a Tokenizer from a serialized vocabulary and list of merges
-        (in the same format that your BPE training code output) and (optionally) a list of special tokens.
+        (in the same format that your BPE training code output) and (optionally) a list of special tokens. 
         """
         pass
 
@@ -33,15 +38,15 @@ class Tokenizer():
         tokenized_string: list[bytes] = []
         for token in pretokenized_string:
             token_list = list(token)
-            j = 0
-            while j < len(token_list) - 1:
-                if (token_list[j], token_list[j + 1]) in self.merges:
-                    merged_token = token_list[j] + token_list[j + 1]
-                    token_list = token_list[:j] + [merged_token] + token_list[j + 2:]
+            i = 0
+            while i < len(token_list) - 1:
+                if (token_list[i], token_list[i + 1]) in self.merges:
+                    merged_token = token_list[i] + token_list[i + 1]
+                    token_list = token_list[:i] + [merged_token] + token_list[i + 2:]
                 else:
-                    j += 1
-            print(f"token list is {token_list}")
-            print(f"tokenized string is {tokenized_string}")
+                    i += 1
+            print(f"the token list is {token_list}")
+            print(f"the current tokenized string is {tokenized_string}")
             tokenized_string.extend(token_list)
 
         encoded_string = []
@@ -53,22 +58,22 @@ class Tokenizer():
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         """
-        Given an iterable of strings (e.g., a Python file handle), return a
-        generator that lazily yields token IDs. This is required for
-        memory-eﬀicient tokenization of large files that we cannot directly
+        Given an iterable of strings (e.g., a Python file handle), return a 
+        generator that lazily yields token IDs. This is required for 
+        memory-eﬀicient tokenization of large files that we cannot directly 
         load into memory.
         """
         _, pretokenized_string = pretokenize_iter(iterable)
         for token in pretokenized_string:
             token_list = list(token)
-            j = 0
-            while j < len(token_list) - 1:
-                if (token_list[j], token_list[j + 1]) in self.merges:
-                    merged_token = token_list[j] + token_list[j + 1]
-                    token_list = token_list[:j] + [merged_token] + token_list[j + 2:]
+            i = 0
+            while i < len(token_list) - 1:
+                if (token_list[i], token_list[i + 1]) in self.merges:
+                    merged_token = token_list[i] + token_list[i + 1]
+                    token_list = token_list[:i] + [merged_token] + token_list[i + 2:]
                 else:
-                    yield token_list[j]
-                    j += 1
+                    yield token_list[i]
+                    i += 1
 
             # for new_token in token_list:
             #     yield self.reverse_vocab[new_token]
